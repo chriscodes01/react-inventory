d8bcdb86ead010acbb670dd72067761cb2c2c23e
chriscodes01 <chriscodes.business@gmail.com>
2023-04-21 07:10:02 -0500

    added props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import './App.css';
+import './App.css'
+import { PropTypes } from "prop-types"
 import Info from "./Info.js"
 
 function App() {
   return (
     <div className="App">
-      <Info />
-      <AddItem />
-      <AddItem />
-      <AddItem />
+      <Info title="Inventory" />
+      <AddItem text="Chris" number={1} />
+      <AddItem text="is" number={2} />
+      <AddItem text="cool" number={3} />
     </div>
   );
 }
 
-function AddItem() {
+function AddItem(props) {
+  const value = props.text
+
   return (
     <form>
+      <p>{props.number}</p>
       <label for="text-form">Type something: </label>
-      <input type="text" id="text-form"/>
+      <input type="text" value={value} id="text-form"/>
     </form>
   )
 }
-export default App;
+
+AddItem.defaultProps = {
+  text: "default",
+  number: 0
+}
+
+AddItem.propTypes = {
+  text: PropTypes.string,
+  number: PropTypes.number
+}
+
+export default App
--- src/Info.js
+++ src/Info.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React from "react"
+import { PropTypes } from "prop-types"
 
 class Info extends React.Component {
+    constructor(props) {
+      super(props);
+      console.log(props);
+    }
+
     render() {
-        const title = "This is my title.";
-        const showTitle = false;
+        const title = this.props.title;
+        // const showTitle = false;
     
         return (
             <div>
-              <h1>Inventory System</h1>
+              <h1>{ title }</h1>
               <p>Manage your stuff.</p>
             </div>
           )
     }
+}
+
+Info.defaultProps = {
+  title: "Title"
+}
+
+Info.propTypes = {
+  title: PropTypes.string
 }
 
   export default Info
