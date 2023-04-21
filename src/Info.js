d68a3a8b475add9d96f22c91fb3c9788887a506a
chriscodes01 <chriscodes.business@gmail.com>
2023-04-20 20:54:55 -0500

    first commit

---
+++ src/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Info extends React.Component {
+    render() {
+        const title = "This is my title.";
+        const showTitle = false;
+    
+        return (
+            <div>
+              <h1>Inventory System</h1>
+              <p>Manage your stuff.</p>
+            </div>
+          )
+    }
+}
+
+  export default Info
